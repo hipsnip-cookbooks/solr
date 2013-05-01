47481fab4424680ea13b9fdbb82a002e5f50b13e
Remy Loubradou <remyloubradou@gmail.com>
2013-05-01 12:41:02 +0100

    NEW: Use hipsnip-solr v0.5.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email "adam@hipsnip.com/remy@hipsnip.com"
 license          "Apache 2.0"
 description      "Installs/Configures Solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+supports 		 'ubuntu', ">= 12.04"
 version          "0.3.0"
 
-depends "hipsnip-jetty" , ">= 0.4.0"
+depends "hipsnip-jetty" , ">= 0.5.0"
