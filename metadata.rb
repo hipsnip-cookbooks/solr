2d354c0383340c5380039f31df7d840e809ffa34
Remy Loubradou <remyloubradou@gmail.com>
2013-05-13 12:37:38 +0100

    NEW: Update to hipsnip-jetty 0.8.0

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports 		 'ubuntu', ">= 12.04"
 version          "0.3.2"
 
-depends "hipsnip-jetty" , ">= 0.6.0"
+depends "hipsnip-jetty", "~> 0.8.0"
