b2b9ef43cb6054edb23594b55be2b1b2dbabdbe8
Remy Loubradou <remyloubradou@gmail.com>
2013-11-20 18:41:55 +0000

    RELEASE: v0.4.3

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          "Apache 2.0"
 description      "Installs/Configures Solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports 		 'ubuntu', ">= 12.04"
-version          "0.4.2"
+version          "0.4.3"
 
 depends "hipsnip-jetty", "~> 0.9.0"
